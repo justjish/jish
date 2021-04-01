9f38d987bbc0816409b92cac16ccdd4749547a3b
sujishpatel <justjish@gmail.com>
2021-03-31 23:16:14 -0400

    performance enchancement

--- src/components/BrainSkill.tsx
+++ src/components/BrainSkill.tsx
@@ -1,10 +1,34 @@
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 import { a, SpringValue, useSpring } from 'react-spring';
 import { SkillItem } from 'data/BrainData';
 import randomColor from 'randomcolor';
 import { css } from '@emotion/react';
 import { box } from 'styles/box.style';
 import { isEven } from 'functions/utils';
+
+const gridBox = css`
+  ${box}
+  display: grid;
+  grid-template-columns: 0.2fr 0.8fr;
+  gap: 0.4rem;
+`;
+const flexBox = css`
+  ${box}
+  display: flex;
+`;
+const futuraFont = css`
+  font-family: futura-pt, sans-serif;
+  font-weight: 500;
+  font-style: normal;
+  text-transform: uppercase;
+`;
+
+const textStyle = (size: number) => css`
+  ${futuraFont};
+  font-size: ${(50 * size) / 100}px;
+`;
+
+const iconStyle = (size: number) => css`height: ${size}px; width: ${size}px;`;
 
 export const BrainSkill: FC<SkillItem & { offset: SpringValue<number> }> = ({
   Icon = null,
@@ -17,7 +41,7 @@ export const BrainSkill: FC<SkillItem & { offset: SpringValue<number> }> = ({
   const [{ y, color }] = useSpring(
     {
       y: offset.to([0, 2, 2.75], [5000, 0, -1000]),
-      from: {y: 5000, color:randomColor({ seed: type, luminosity: 'bright', alpha: 0.5 })},
+      from: { y: 5000, color: randomColor({ seed: type, luminosity: 'bright', alpha: 0.5 }) },
       config: {
         mass: (100 - size + (isEven(idx) ? -idx : idx)) / 25,
         tension: 50,
@@ -27,29 +51,15 @@ export const BrainSkill: FC<SkillItem & { offset: SpringValue<number> }> = ({
     [],
   );
 
-  return (
-    <a.div css={box} style={{ y, color}}>
-      {Icon && (
-        <Icon
-          title={desc}
-          css={css`
-            height: ${size}px;
-            width: ${size}px;
-          `}
-        />
-      )}
+  // Wrapping styles in useMemo for performance optimizations on lower end devices.
+  const cssBox = useMemo(() => (Icon ? gridBox : flexBox), [Icon]);
+  const cssIcon = useMemo(() => iconStyle(size), [size]);
+  const cssText = useMemo(() => textStyle(size), [size]);
 
-      <div
-        css={css`
-          font-size: ${(50 * size) / 100}px;
-          font-family: futura-pt, sans-serif;
-          font-weight: 500;
-          font-style: normal;
-          text-transform: uppercase;
-        `}
-      >
-        {desc}
-      </div>
+  return (
+    <a.div css={cssBox} style={{ y, color }}>
+      {Icon && <Icon title={desc} css={cssIcon} />}
+      <div css={cssText}>{desc}</div>
     </a.div>
   );
 };
