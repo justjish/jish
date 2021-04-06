a65b3dfaacdde99f970d8f1398888639c6794cd0
sujishpatel <justjish@gmail.com>
2021-04-06 00:02:44 -0400

    initial setup

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,19 @@
-import * as functions from "firebase-functions";
-
+import { https, logger as log } from "firebase-functions";
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+const typeDefs = gql`
+  type Skill {
+    name: String;
+  }
+  type Query {
+    skills: [Skill]
+  }
+`;
+
+export const helloWorld = https.onRequest((_, res) => {
+  log.info("Hello logs!", {structuredData: true});
+  res.send("Hello from Firebase!");
 });
