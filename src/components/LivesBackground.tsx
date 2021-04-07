b0206d95ee59ef9de042cd9e5dd124a0843aa93c
sujishpatel <justjish@gmail.com>
2021-04-07 14:09:19 -0400

    tiny edit

--- src/components/LivesBackground.tsx
+++ src/components/LivesBackground.tsx
@@ -4,17 +4,15 @@ import { row } from 'styles/row.style';
 import svgFrame from 'assets/graphics/cityFrameCircle.svg';
 import { css } from '@emotion/react';
 
-export const LivesBackground: FC<{ scale: SpringValue<1 | 3>; y: SpringValue<number> }> = ({ scale, y }) => {
-  return (
-    <div css={row}>
-      <a.img
-        css={css`
-          width: 100vw;
-          position: relative;
-        `}
-        style={{ scale, y }}
-        src={svgFrame}
-      />
-    </div>
-  );
-};
+export const LivesBackground: FC<{ scale: SpringValue<1 | 3>; y: SpringValue<number> }> = ({ scale, y }) => (
+  <div css={row}>
+    <a.img
+      css={css`
+        width: 100vw;
+        position: relative;
+      `}
+      style={{ scale, y }}
+      src={svgFrame}
+    />
+  </div>
+);
