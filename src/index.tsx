dc45eebc4aef6a04f225483b8d91a166a0964550
Sujish Patel <sujishpatel@protonmail.com>
2022-04-09 05:12:14 -0400

    Minor edits

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,4 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from 'apps/App';
-// Lets not assume our initial element exists...
 const root = createRoot(document.getElementById('root') ?? document.createElement('div'));
 root.render(<App />);
