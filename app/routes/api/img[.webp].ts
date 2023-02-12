55ff06c5c9894f27d432a15692278ad3b58f8ec0
Sujish Patel <justjish@gmail.com>
2023-02-12 02:01:51 -0500

    test

--- app/routes/api/img[.webp].ts
+++ app/routes/api/img[.webp].ts
@@ -2,16 +2,24 @@ import type { LoaderFunction } from '@remix-run/cloudflare';
 
 const url = new URL('https://upload.wikimedia.org/wikipedia/commons/3/3d/LARGE_elevation.jpg');
 export const loader: LoaderFunction = async ({ request, context }) => {
-  const resize = await fetch(url.href, {
-    cf: { image: { width: 300, format: 'webp', fit: 'scale-down' } },
-    headers: request.headers,
-  });
+  try {
+    const resize = await fetch(url.href, {
+      cf: { image: { width: 300, format: 'webp', fit: 'scale-down' } },
+      headers: request.headers,
+    });
 
-  return new Response(resize.body, {
-    headers: {
-      'Content-Type': 'image/webp',
-    },
-  });
+    return new Response(resize.body, {
+      headers: {
+        'Content-Type': 'image/webp',
+      },
+    });
+  } catch (e) {
+    if (e instanceof Error) {
+      return new Response(JSON.stringify(e.message));
+    } else {
+      return new Response('No Error');
+    }
+  }
   // console.log('assetUrl', assetUrl);
   // const assetRes = context.ASSETS.fetch(assetUrl, { headers: request.headers });
   // console.log('assetRes', assetRes);
