0d97bd96f341e3a57eaaa99422a51bec3903d2de
Sujish Patel <justjish@gmail.com>
2023-02-11 22:46:45 -0500

    attempt to trigger a resize

--- app/components/HelloProfile.tsx
+++ app/components/HelloProfile.tsx
@@ -2,13 +2,14 @@ import type { FC } from 'react';
 import type { SpringValue } from '@react-spring/web';
 import { a } from '@react-spring/web';
 import fbSDWebp from '~/assets/pictures/fullbody@0.25x.webp';
+import fbHDPng from '~/assets/pictures/fullbody@0.5x.png';
 import { Image } from 'remix-image-cloudflare';
 const AnimatedImage = a(Image);
 export const HelloProfile: FC<{ opacity: SpringValue<number>; x: SpringValue<number> }> = ({ opacity, x }) => {
   return (
     <AnimatedImage
       className={'object-scale-down w-[120vw] sm:w-[90vw] lg:w-[50vw] 2xl:w-[40vw]'}
-      src={fbSDWebp}
+      src={fbHDPng}
       responsive={[
         {
           size: {
