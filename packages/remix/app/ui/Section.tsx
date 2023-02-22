c1f238ad6052451b61843122ef243a3688eb02a1
Sujish Patel <justjish@gmail.com>
2023-02-22 03:17:26 -0500

    fix fonts

--- packages/remix/app/ui/Section.tsx
+++ packages/remix/app/ui/Section.tsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect, type FC, type ReactNode, forwardRef } from 'react';
+import { useRef, useEffect, type FC, type ReactNode, forwardRef, useTransition } from 'react';
 import { mergeRefs } from 'react-merge-refs';
 import useMeasure from 'react-use-measure';
 import { useMenuState } from '~/hooks/useMenu';
@@ -7,8 +7,9 @@ export type SectionProps = { children: ReactNode; sectionKey: SectionType };
 export const Section = forwardRef<HTMLDivElement, SectionProps>(({ children, sectionKey }, boundsRef) => {
   const [ref, bounds] = useMeasure();
   const localRef = useRef<HTMLDivElement>(null);
+  const [isPending, startTransition] = useTransition()
   const { set } = useMenuState()[sectionKey];
-  useEffect(() => set({ ...bounds, absoluteTop: localRef.current?.offsetTop ?? 0 }), [bounds, set]);
+  useEffect(() => startTransition(()=>set({ ...bounds, absoluteTop: localRef.current?.offsetTop ?? 0 })), [bounds, set]);
   return (
     <div
       className={'box-border h-screen flex overflow-hidden items-center justify-center flex-col p-5'}
